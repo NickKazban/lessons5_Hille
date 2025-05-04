6e75f8879bc85e427ca5fe3c46da79af57cf2582
NickKazban <mykola.kazban@gmail.com>
2025-05-04 17:45:01 +0300

    Use '/retuen'/ in function

--- file.js
+++ file.js
@@ -54,17 +54,76 @@
 
 // Function Expression
 
-var prompt = require('prompt');                                   //стороння бібліотека prompt
+// var prompt = require('prompt');                                   //стороння бібліотека prompt
 
-let squareArea = function () {
-    prompt.start();                                                //стартує біліотека звідси
-    let side = prompt.get(["side"], function (err, result) {
-           console.log("side:", result.side);
-           console.log("side: " + result.side);
-           let side =  result.side;
-           console.log(side * side);
-    });
-}
-squareArea();
+// let squareArea = function () {
+//     prompt.start();                                                //стартує біліотека звідси
+//     let side = prompt.get(["side"], function (err, result) {
+//            console.log("side:", result.side);
+//            console.log("side: " + result.side);
+//            let side =  result.side;
+//            console.log(side * side); 
+//     });
+// }
+// squareArea();
+
+
+////////////////corectFunctiont///////////////
+
+
+// printGreeting(); // І в нас ніяких проблем;
+// function printGreeting() {
+//     console.log("Hello world")
+// }
+////////////////var vs  let in the function ///////////////
+
+// printGreeting()
+// var printGreeting = function () {
+//     console.log("Hello world")
+// }
+
+// // Function Expression корисно використовувати коли ми хочемо в змінній зберігати різні функції
+// let autoBrand = "ТАВРІЯ";
+// let accelerateСar;
+// switch (autoBrand) {
+//     case "Audi":
+//         accelerateСar = function () {
+//             let audiSpeedIndex = 100;
+//             console.log("Розганяємо авто до 150км: " + audiSpeedIndex * 1.5);
+//         }
+//         break;
+//     case "ТАВРІЯ":
+//         accelerateСar = function () {
+//             let audiSpeedIndex = 0;
+//             console.log("Розганяємо авто до 150км")
+//             for (let i = 0; i < 60; i += 10) {
+//                 audiSpeedIndex += i;
+//                 console.log(audiSpeedIndex);
+//             }
+//         }
+//         break;
+// }
+
+// accelerateСar();
+
+/////////////////////////////Обовґязкове використання РЕТУРН у функціях для повернення значення. ДЛя допалтших операцій з ними /////////////////////////////
+
+// Поняття return
+// Розглянемо що повертає функція за замовчуванням
+// let a = 5;
+
+// function squareAreaCalculation() {
+//     let result = a * a; // Але все одно погано що ми маємо залежність через використання зовнішньої змінної:
+//     return result;
+// }
+//  var result = squareAreaCalculation();
+//  var result2=result+'Hello';
+
+ 
+//  console.log('Перший результат буде число:'+ result);
+//  console.log('Другий результат буде рядок:'+ result2);
+//  console.log("Якщо ж визначити, який тип вони мають тоді")
+//  console.log('Перше значення буде мати тип значення :'+ typeof(result));
+//  console.log('Друге значення буде мати тип значення :'+ typeof(result2));
 
 
